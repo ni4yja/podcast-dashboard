055d6a456151e4698c6f229ec2bf9bb3c2e70333
ni4yja <k2.liuba@gmail.com>
2021-10-14 15:17:02 +0300

    home sidebar finished

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
       style="background-color: #f2f8fd"
       class="pa-14"
     >
-      <div class="text-center mt-16">
+      <div class="text-center mt-10">
         <v-img
           :src="require(`~/assets/liuba.png`)"
           alt="profile-picture"
@@ -36,6 +36,28 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <v-toolbar flat dense color="rgba(0,0,0,0)" class="mt-6">
+        <v-toolbar-title class="subheading ml-n4">Balance</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <span class="headline font-weight-light mr-n4 mb-n4">$127</span>
+      </v-toolbar>
+      <div>
+        <a href="#" style="color:#e35376">Add funds</a>
+        <v-icon color="#e35376" size="20">mdi-chevron-right</v-icon>
+      </div>
+      <v-card class="mt-11 rounded-xl" flat color="#e35376">
+        <v-card-subtitle style="color: #fff">Anything to tell the audience?</v-card-subtitle>
+        <v-card-text class="headline mt-3" style="color: #fff">Become <br /> <strong>a podcaster!</strong></v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <a href="#" style="color:#fff">Learn more</a>
+          <v-icon color="#efff" size="20">mdi-chevron-right</v-icon>
+        </v-card-actions>
+      </v-card>
+      <div class="mt-12 d-flex justify-center">
+        <v-icon class="mr-13">mdi-cog-outline</v-icon>
+        <v-icon>mdi-exit-to-app</v-icon>
+      </div>
     </v-navigation-drawer>
   </v-app>
 </template>
