94a6f48252fee16c4f5c5810a13dc01fbf9e412f
ni4yja <k2.liuba@gmail.com>
2021-10-14 19:01:41 +0300

    navigation added

---
+++ components/Navigation.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav>
+    <v-list>
+      <v-item-group>
+        <v-item link class="menu-link mr-12"
+          ><NuxtLink to="/">Home</NuxtLink></v-item
+        >
+        <v-item link class="menu-link mr-12"
+          ><NuxtLink to="/popular">Popular</NuxtLink></v-item
+        >
+        <v-item link class="menu-link mr-12"
+          ><NuxtLink to="/search">Search</NuxtLink></v-item
+        >
+      </v-item-group>
+    </v-list>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.v-application .menu-link {
+  color: #000;
+  text-decoration: none;
+  position: relative;
+}
+
+.menu-link::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: -7px;
+  width: 20px;
+  height: 3px;
+  background: #e35176;
+  display: none;
+}
+
+.menu-link.nuxt-link-active::after {
+  display: block;
+}
+</style>
---
+++ pages/popular.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Popular</h1>
+</template>
+
+<script>
+export default {
+  layout: 'HomeHeader'
+}
+</script>
+
+<style>
+
+</style>
