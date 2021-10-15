bdf80ed36bcb9be7498d5d3e151d2c3434ce7ac7
ni4yja <k2.liuba@gmail.com>
2021-10-15 15:44:27 +0300

    home layout started

---
+++ components/HomeSearch.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="search">
+    <input id="serch" type="text" class="rounded-xl" v-on:blur="handleBlur">
+    <label for="search"><v-icon color="black" size="30">mdi-magnify</v-icon></label>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleBlur(e) {
+      if (e.target.value.trim().length) {
+        e.target.classList.add("blurred");
+      } else {
+        e.target.classList.remove("blurred");
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.search {
+  position: relative;
+}
+
+.search input {
+  border: 2px solid black;
+  width: 100%;
+  height: 84px;
+  padding: 1rem 1rem 1rem 3rem;
+  outline: none;
+  color: transparent;
+}
+
+.search label {
+  position: absolute;
+  left: calc(50% - 15px);
+  bottom: calc(50% - 15px);
+  transition: transform ease .3s;
+}
+
+.search input:focus {
+  color: #000;
+  transition: color 0s ease .3s;
+}
+
+.search input.blurred {
+  color: #000;
+}
+
+.search input:focus + label {
+  transform: translateX(-120px);
+}
+
+.search input.blurred + label {
+  transform: translateX(-120px);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,97 +1,53 @@
 <template>
   <div>
     <HomeHeader />
-    <v-navigation-drawer
-      absolute
-      permanent
-      floating
-      right
-      width="380"
-      style="background-color: #f2f8fd"
-      class="pa-14"
-    >
-      <div class="text-center mt-10">
-        <v-img
-          :src="require(`~/assets/liuba.png`)"
-          alt="profile-picture"
-          width="100"
-          class="ml-auto mr-auto"
-        ></v-img>
-        <h2 class="mt-6 font-weight-medium">Liuba Kuibida</h2>
-      </div>
-      <v-list class="mt-11 pa-0">
-        <v-list-item class="pa-0" v-for="(item, i) in items" :key="i">
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title
-              v-text="item.text"
-              class="font-weight-medium mb-2"
-            ></v-list-item-title>
-            <v-list-item-subtitle
-              v-text="item.sub"
-              class="grey--text"
-            ></v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <v-toolbar flat dense color="rgba(0,0,0,0)" class="mt-6">
-        <v-toolbar-title class="subheading ml-n4">Balance</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <span class="headline font-weight-light mr-n4 mb-n4">$127</span>
-      </v-toolbar>
-      <div>
-        <a href="#" style="color: #e35376">Add funds</a>
-        <v-icon color="#e35376" size="20">mdi-chevron-right</v-icon>
-      </div>
-      <v-card class="mt-11 rounded-xl" flat color="#e35376">
-        <v-card-subtitle style="color: #fff"
-          >Anything to tell the audience?</v-card-subtitle
-        >
-        <v-card-text class="headline mt-3" style="color: #fff"
-          >Become <br />
-          <strong>a podcaster!</strong></v-card-text
-        >
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <a href="#" style="color: #fff">Learn more</a>
-          <v-icon color="#efff" size="20">mdi-chevron-right</v-icon>
-        </v-card-actions>
-      </v-card>
-      <div class="mt-12 d-flex justify-center">
-        <v-icon class="mr-13">mdi-cog-outline</v-icon>
-        <v-icon>mdi-exit-to-app</v-icon>
-      </div>
-    </v-navigation-drawer>
+    <HomeSidebar />
+    <v-main style="max-width: 910px" class="pr-13">
+      <v-container fluid class="mt-6 pa-0">
+        <v-row no-gutters>
+          <v-col class="mr-15">
+            <HomeSearch />
+            <v-row class="mt-3">
+              <v-col>
+                <v-card class="rounded-xl" flat color="#4d54e1">
+                  <v-card-subtitle style="color: #fff">Online?</v-card-subtitle>
+                  <v-card-text class="mt-n3" style="color: #fff"
+                    >217 <strong>podcasts</strong></v-card-text
+                  >
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card
+                  class="rounded-xl d-flex justify-center align-center"
+                  flat
+                  color="#f9d11d"
+                  style="height: 100%"
+                >
+                  <v-card-title>Trends</v-card-title>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col style="border: 2px solid blue">
+            <div style="width: 370px"></div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
   </div>
 </template>
 
 <script>
 import HomeHeader from "~/components/HomeHeader.vue";
+import HomeSidebar from "~/components/HomeSidebar.vue";
+import HomeSearch from "~/components/HomeSearch.vue";
 
 export default {
   components: {
     HomeHeader,
+    HomeSidebar,
+    HomeSearch,
   },
-  data: () => ({
-    items: [
-      {
-        icon: "mdi-account-supervisor-outline",
-        text: "Following podcasters",
-        sub: "14 podcasters",
-      },
-      {
-        icon: "mdi-playlist-plus",
-        text: "Saved podcasts",
-        sub: "14 podcasters",
-      },
-      {
-        icon: "mdi-history",
-        text: "History",
-      },
-    ],
-  }),
 };
 </script>
 
